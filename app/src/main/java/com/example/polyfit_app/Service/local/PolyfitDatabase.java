bb31afa13f53dc5c4822ac69738bef59425a1866
quocphuvt <phu@uhouse.asia>
2019-10-06 14:08:15 +0700

    Setting up Room

--- app/src/main/java/com/example/polyfit_app/Service/local/PolyfitDatabase.java
+++ app/src/main/java/com/example/polyfit_app/Service/local/PolyfitDatabase.java
@@ -1,4 +1,30 @@
 package com.example.polyfit_app.Service.local;
 
-public class PolyfitDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.polyfit_app.Model.User;
+
+@Database(entities = {User.class}, version = 1)
+
+public abstract class PolyfitDatabase extends RoomDatabase {
+    private static final String DB_NAME = "polyfit_db";
+    private static PolyfitDatabase instance;
+
+    public static synchronized PolyfitDatabase getInstance(Context context) {
+        if(instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), PolyfitDatabase.class, DB_NAME)
+            .fallbackToDestructiveMigration()
+            .build();
+        }
+
+        return instance;
+    }
+
+    public abstract UserDAO  userDAO();
 }
--- app/src/main/java/com/example/polyfit_app/Service/local/UserDAO.java
+++ app/src/main/java/com/example/polyfit_app/Service/local/UserDAO.java
@@ -1,4 +1,18 @@
 package com.example.polyfit_app.Service.local;
 
-public class UserDAO {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.polyfit_app.Model.User;
+
+import java.util.List;
+
+@Dao
+public interface UserDAO {
+    @Query("SELECT * FROM  polyfit_users")
+    List<User> getAllUsers();
+
+    @Insert
+    void registerUser(User user);
 }
