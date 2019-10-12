f237f2dde502e4c5944f3f40c1da82019304ff3b
PhuongTrinh2207 <44720814+PhuongTrinh2207@users.noreply.github.com>
2019-10-12 18:30:20 +0700

    dishes list

--- app/src/main/java/com/example/polyfit_app/Fragments/DishesFragment.java
+++ app/src/main/java/com/example/polyfit_app/Fragments/DishesFragment.java
@@ -1,5 +1,9 @@
 package com.example.polyfit_app.Fragments;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,31 +12,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.polyfit_app.Adapter.DishesAdapter;
+import com.example.polyfit_app.Adapter.ExcerciseAdapter;
 import com.example.polyfit_app.Model.Dishes;
+import com.example.polyfit_app.Model.Excercise;
 import com.example.polyfit_app.R;
 
 import java.util.ArrayList;
 
-public class DietsFragment extends Fragment {
+public class DishesFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
     ArrayList<Dishes> dishesArrayList;
     RecyclerView recyclerView;
-    private OnFragmentInteractionListener mListener;
 
-    public DietsFragment() {
+    public DishesFragment() {
     }
 
 
-    public static DietsFragment newInstance(String param1, String param2) {
-        DietsFragment fragment = new DietsFragment();
+    public static DishesFragment newInstance(String param1, String param2) {
+        DishesFragment fragment = new DishesFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -52,16 +55,16 @@ public class DietsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.activity_dishes_fragment, container, false);
-        recyclerView = view.findViewById(R.id.viewDishes);
-        dishesArrayList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            Dishes dishes = new Dishes("abc", "hcm");
-            dishesArrayList.add(dishes);
-        }
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
-        setDataForDishesList(dishesArrayList);
+        View view = inflater.inflate(R.layout.activity_dishes_fragment, container, false);
+//        recyclerView = view.findViewById(R.id.viewDishes);
+//        dishesArrayList = new ArrayList<>();
+//        for (int i = 0; i < 10; i++) {
+//            Dishes dishes = new Dishes("abc", "hcm");
+//            dishesArrayList.add(dishes);
+//        }
+//        recyclerView.setHasFixedSize(true);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+//        setDataForDishesList(dishesArrayList);
         return view;
     }
 
@@ -89,9 +92,12 @@ public class DietsFragment extends Fragment {
     }
 
 
+
+
     public interface OnFragmentInteractionListener {
         void onFragmentInteraction(Uri uri);
     }
+
     private void setDataForDishesList(ArrayList<Dishes> dishesList) {
         DishesAdapter dishesAdapter = new DishesAdapter(dishesArrayList, getActivity());
         recyclerView.setAdapter(dishesAdapter);
---
+++ app/src/main/java/com/example/polyfit_app/Model/Dishes.java
@@ -0,0 +1,27 @@
+package com.example.polyfit_app.Model;
+
+public class Dishes {
+    private String title;
+    private String location;
+
+    public Dishes(String title, String location) {
+        this.title = title;
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
