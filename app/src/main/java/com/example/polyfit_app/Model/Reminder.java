3c9acf911dd9053765e48aa2d3d8c5a1cd7bf640
quocphuvt <phu@uhouse.asia>
2019-10-13 14:12:10 +0700

    Resolve

--- app/src/main/java/com/example/polyfit_app/Model/Reminder.java
+++ app/src/main/java/com/example/polyfit_app/Model/Reminder.java
@@ -13,8 +13,25 @@ public class Reminder {
     private int hour;
     @ColumnInfo(name = "minute")
     private int minute;
+    @ColumnInfo(name = "monday")
+    private int monday;
+    @ColumnInfo(name = "tuesday")
+    private int tuesday;
+    @ColumnInfo(name = "wednesday")
+    private int wednesday;
+    @ColumnInfo(name = "thursday")
+    private int thursday;
+    @ColumnInfo(name = "friday")
+    private int friday;
+    @ColumnInfo(name = "saturday")
+    private int saturday;
+    @ColumnInfo(name = "sunday")
+    private int sunday;
     @ColumnInfo(name = "note")
     private String note;
+
+    public Reminder() {
+    }
 
     @Ignore
     public Reminder(int hour, int minute, String note) { //Insert
@@ -23,11 +40,97 @@ public class Reminder {
         this.note = note;
     }
 
+    @Ignore
     public Reminder(int id, int hour, int minute, String note) { //Get
         this.id = id;
         this.hour = hour;
         this.minute = minute;
         this.note = note;
+    }
+
+    @Ignore
+    public Reminder(int id, int hour, int minute, int monday, int tuesday, int wednesday, int thursday, int friday, int saturday, int sunday, String note) {
+        this.id = id;
+        this.hour = hour;
+        this.minute = minute;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+        this.note = note;
+    }
+
+    @Ignore
+    public Reminder(int hour, int minute, int monday, int tuesday, int wednesday, int thursday, int friday, int saturday, int sunday, String note) {
+        this.hour = hour;
+        this.minute = minute;
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+        this.note = note;
+    }
+
+    public int getMonday() {
+        return monday;
+    }
+
+    public void setMonday(int monday) {
+        this.monday = monday;
+    }
+
+    public int getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(int tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public int getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(int wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public int getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(int thursday) {
+        this.thursday = thursday;
+    }
+
+    public int getFriday() {
+        return friday;
+    }
+
+    public void setFriday(int friday) {
+        this.friday = friday;
+    }
+
+    public int getSaturday() {
+        return saturday;
+    }
+
+    public void setSaturday(int saturday) {
+        this.saturday = saturday;
+    }
+
+    public int getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(int sunday) {
+        this.sunday = sunday;
     }
 
     public int getId() {
