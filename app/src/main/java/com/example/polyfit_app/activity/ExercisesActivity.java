99f5a582f15f62aa3843af0429cb67b746011bcc
quocphuvt <phu@uhouse.asia>
2019-12-10 13:17:53 +0700

    clean up code

--- app/src/main/java/com/example/polyfit_app/activity/ExercisesActivity.java
+++ app/src/main/java/com/example/polyfit_app/activity/ExercisesActivity.java
@@ -114,7 +114,6 @@ public class ExercisesActivity extends AppCompatActivity implements ItemClickLis
         i.putExtra("id", id);
         i.putExtra("part", getSupportActionBar().getTitle());
         i.putExtra("bodyPartId", bodyPartId);
-        i.putExtra("isLoadingByLevel", false);
         startActivity(i);
     }
 
